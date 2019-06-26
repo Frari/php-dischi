7f3dade95be278b0b7b985c1262f71624d1b4897
Frari <francescoriccardi86@gmail.com>
2019-06-26 16:27:28 +0200

    problema viusalizzazione query

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,13 @@ $(document).ready(function(){
     'method':'GET',
     'success': function(data){
       var dischi = JSON.parse(data);
+      // funzione per ordinare gli album per data
+      obj_db.sort(function (a, b) {
+           return a.year.localeCompare(b.year) //<-- compara le stringhe
+         });
+
+         console.log(obj_db);
+      // stampare le schede
       for (var i = 0; i < dischi.length; i++) {
         var disco = dischi[i];
         var context = {
@@ -41,5 +48,8 @@ $(document).ready(function(){
         $(this).hide();
       }
     })
-  })
+  });
+  $('.but_sel_artista').click(function(){
+    alert('ok');
+  });
 });
